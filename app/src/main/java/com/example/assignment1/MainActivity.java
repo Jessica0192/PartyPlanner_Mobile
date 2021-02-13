aa1d414ce3fe867b1ecd721beed5576f5fa4120a
sijia sun <suka_0923@hotmail.com>
2021-02-13 09:23:16 -0500

    Removed the second activity_main.xml

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -11,15 +11,34 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnLogin = null;
 
+    /*
+     * FUNCTION: onCreate
+     * DESCRIPTION:
+     *      This function is going to be called as default when this page is loaded and there are
+     *      several functions and listeners that does some actions
+     * PARAMETER:
+     *      Bundle savedInstanceState: save instance state
+     * RETURNS:
+     *      void: there's no return value
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
+            /*
+             * FUNCTION: onClick
+             * DESCRIPTION:
+             *      This function is called when Let's Party button is clicked
+             * PARAMETER:
+             *      View v: the view within the AdapterView that was clicked
+             * RETURNS:
+             *      void: there's no return value
+             */
             @Override
-            public void onClick(View view) {
-                Intent eventListIntent = new Intent(view.getContext(), EventListActivity.class);
+            public void onClick(View v) {
+                Intent eventListIntent = new Intent(v.getContext(), EventListActivity.class);
                 startActivity(eventListIntent);
             }
         });

6ba62415abf2729aa4203917ef8f50f6766e6ab8
sijia sun <suka_0923@hotmail.com>
2021-04-20 11:30:34 -0400

    Updated map

--- app/src/main/java/com/example/assignment1/MapsActivity.java
+++ app/src/main/java/com/example/assignment1/MapsActivity.java
@@ -17,69 +17,35 @@ import com.google.android.gms.maps.MapFragment;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 //
-//    private GoogleMap mMap;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//    }
-//
-//    /**
-//     * Manipulates the map once available.
-//     * This callback is triggered when the map is ready to be used.
-//     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-//     * we just add a marker near Sydney, Australia.
-//     * If Google Play services is not installed on the device, the user will be prompted to install
-//     * it inside the SupportMapFragment. This method will only be triggered once the user has
-//     * installed Google Play services and returned to the app.
-//     */
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//    }
-    static final LatLng AddressSet = new LatLng(43.466667, -80.516670);
-    private GoogleMap googleMap;
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        try {
-            if (googleMap == null) {
-                FragmentManager manager = getFragmentManager();
-                MapFragment fragment = (MapFragment)manager.findFragmentById(R.id.map);
-                fragment.getMapAsync(this);
-            }
-//            CameraUpdate point = CameraUpdateFactory.newLatLng(AddressSet);
-//            this.map.moveCamera(point);
-//            this.map.animateCamera(point);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-        if (this.googleMap != null) {
-            this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-//            CameraUpdate point = CameraUpdateFactory.newLatLng(AddressSet);
-//            this.map.moveCamera(point);
-//            this.map.animateCamera(point);
-            Marker AS = this.googleMap.addMarker(
-                            new MarkerOptions().position(AddressSet).title("Your Location"));
-        }
+        myMap = googleMap;
+
+        // Add a marker in Waterloo ON and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng YourLocation = new LatLng(43.466667, -80.516670);
+        myMap.addMarker(new MarkerOptions().position(YourLocation).title("Your Location"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(YourLocation));
     }
 }
